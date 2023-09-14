55c1623cb2fd3087c26fd60081f2efc35b0b875c
Rohit Shrivastav <rohitrks805@gmail.com>
2023-09-14 09:23:03 +0530

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+let x = function(e){
+     window.open ('https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?w=740&t=st=1672927029~exp=1672927629~hmac=32a16edf77d2b6d182a5a0d994eb5e6fe0d2ec2b388e7a6c99290ea1636853fd');
+}
+let cv = document.getElementById("btn-cv");
+
+cv.addEventListener('click', x);
+
+let btn_git = function(e){
+     window.open('https://github.com/STRANJaR');
+}
+let github = document.getElementById("btn_git");
+github.addEventListener('click', btn_git);
